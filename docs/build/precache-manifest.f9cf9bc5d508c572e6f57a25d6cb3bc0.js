411bbc8c6dfa86f800f71b70a247f4b888698b5c
ayushman2095 <ayushman.trivedi@capgemini.com>
2020-03-20 17:44:24 +0530

    1 commit

---
+++ docs/build/precache-manifest.f9cf9bc5d508c572e6f57a25d6cb3bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3363435518bc656a65c5bb418b6e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08842a72d10a1b8a6ddb",
+    "url": "/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "08842a72d10a1b8a6ddb",
+    "url": "/static/js/2.8917577c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.8917577c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdd7d7dbd078f7a35a1",
+    "url": "/static/js/main.8af02871.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
--- src/component/ManageCoursePage.js
+++ src/component/ManageCoursePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
 import { Prompt } from "react-router-dom";
+import * as courseApi from "../api/courseApi";
 
 const ManageCoursePage = props => {
   const [course, setCourse] = useState({
@@ -10,18 +11,26 @@ const ManageCoursePage = props => {
     authorId: null,
     category: ""
   });
-  function HandleChange(event) {
+  function handleChange(event) {
     const updatedCourse = {
       ...course,
       [event.target.name]: event.target.value
     };
     setCourse(updatedCourse);
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course);
+  }
   return (
     <div>
       <h2>Manage Course</h2>
       <Prompt when={true} message="Are you sure?"></Prompt>
-      <CourseForm course={course} onChange={HandleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 };
